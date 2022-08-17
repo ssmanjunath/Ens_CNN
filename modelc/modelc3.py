949dfb160fda21926277fe131da5467cefaee8fd
ssmanjunath <saimanjunaths@protonmail.com>
2022-08-17 22:29:54 +0530

    model to compare

---
+++ modelc/modelc3.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+
+class ModelC3(nn.Module):
+    def __init__(self):
+        super(ModelC3,self).__init__()
+        self.conv1 = nn.Conv2d(1,32,5,bias=False,padding=2)
+        self.conv1_bn = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32,64,5,bias=False,padding=2)
+        self.conv2_bn = nn.BatchNorm2d(64)
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv3 = nn.Conv2d(64,96,5,bias=False,padding=2)
+        self.conv3_bn =nn.BatchNorm2d(96)
+        self.conv4 = nn.Conv2d(96,128,5,bias=False,padding=2)
+        self.conv4_bn = nn.BatchNorm2d(128)
+        self.pool2 = nn.MaxPool2d(2)
+        self.flat1 = nn.Linear(6272,10,bias=False)
+        self.flat1_bn = nn.BatchNorm2d(10)
+
+    def get_logits(self,x):
+        x = (x-0.5)*2.0
+        conv1 = f.relu(self.conv1_bn(self.conv1(x)))
+        conv2 = f.relu(self.conv2_bn(self.conv2(conv1)))
+        pool1 = self.pool1(conv2)
+        conv3 = f.relu(self.conv3_bn(self.conv3(pool1)))
+        conv4 = f.relu(self.conv4_bn(self.conv4(conv3)))
+        pool1 = self.pool1(conv4)
+        flat1 = torch.flatten(pool1.permute(0,2,3,1),1)
+        logits = self.flat1_bn(self.flat1(flat1))
+        return logits
+
+    def forward(self,x):
+        logits = self.get_logits(x)
+        return f.log_softmax(logits,dim=1)
