88c2aa327eaaed50ff64021d4c34c62c4517998a
ssmanjunath <saimanjunaths@protonmail.com>
2022-08-20 11:44:22 +0530

    exponential moving average for param_updates

---
+++ ema.py
@@ -0,0 +1,10 @@
+# Exponential moving average implemented in shadow copy
+class EMA:
+    def __init__(self,model,decay):
+        pass
+    def register(self,model,num_of_updates):
+        pass
+    def assign(self,model,params):
+        pass
+    def resume(self,model,value):
+        pass
