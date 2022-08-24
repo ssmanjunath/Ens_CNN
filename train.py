65a8a19b42d171ec9ca5ce0afd6f35dae54de623
ssmanjunath <saimanjunaths@protonmail.com>
2022-08-24 23:16:18 +0530

    training

--- train.py
+++ train.py
@@ -14,4 +14,25 @@ from mnist import mnist
 from transforms import Randomrotate
 from modelm.modelm3 import ModelM3
 from modelm.modelm5 import ModelM5
-from modelm.modelm7 import ModelM5
+from modelm.modelm7 import ModelM7
+
+def run(seed = 0, kernelsize = 5, epochs = 150, logdir = "tmp"):
+    # random seed
+    SEED = seed
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
+    np.random.seed(SEED)
+    
+    #variables
+    kernel_size = kernelsize
+    num_epochs = epochs
+
+    #log path
+    if not os.path.exists(f"../logs/{logdir}"):
+        os.makedirs(f"../logs/{logdir}") 
+    OUTPUT_FILE = str(f"../logs/{logdir}/log{SEED:03d}.out")
+    MODEL_FILE = str(f"../logs/{logdir}/model{SEED:03d}.pth")
+    
+
