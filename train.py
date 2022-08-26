69d45e0c241695401ee9db4d985e5aa73a6997d6
ssmanjunath <saimanjunaths@protonmail.com>
2022-08-26 18:10:17 +0530

    update

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import os 
 import argparse
+import torch.optim as optim
 import sys
 import numpy as np
 import math
 import torch
 import torch.nn as nn
-import torch.nn.functional as f
+import torch.nn.functional as F
 from torchvision import datasets,transforms
 from torchsummary import summary
 from PIL import Image
@@ -35,4 +36,59 @@ def run(seed = 0, kernelsize = 5, epochs = 150, logdir = "tmp"):
     OUTPUT_FILE = str(f"../logs/{logdir}/log{SEED:03d}.out")
     MODEL_FILE = str(f"../logs/{logdir}/model{SEED:03d}.pth")
     
+    #device to run nn
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    if use_cuda == False:
+        print('Warning: CPU is used for training')
+        exit(0)
+    
+    #Data Augmentation
+    transform = transforms.Compose([Randomrotate(20,seed =SEED),
+                transforms.RandomAffine(0,translate=(0.2,0.2))])
+    
+    #Load dataset
+    test_dataset = mnist(training=False,transform=None)
+    train_dataset = mnist(training=True,transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_dataset,batch_size = 120, shuffle= True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = 100, shuffle= False)
 
+    #model selection 
+    if kernel_size ==3:
+        model = ModelM3.to(device=device)
+    elif kernel_size==5:
+        model = ModelM5.to(device=device)
+    else:
+        model = ModelM7.to(device=device)
+    
+    #Hyperparameter optimisation
+    ema = EMA(model,decay=0.999)
+    optimiser = optim.Adam(model.parameters() ,lr=0.001)
+    lr_scheduler = optim.lr_scheduler.ExponentialLR(optimiser,gamma=0.98)
+
+    #out file 
+    f = open(OUTPUT_FILE,'W')
+    f.close()
+    
+    #global var
+    g_step = 0
+    max_correct = 0
+
+    #training
+    for epoch in range(num_epochs):
+        model.train()
+        train_loss = 0
+        train_corr = 0
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data,target = data.to(device), target.to(device,torch.int64)
+            optimiser.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output,target)
+            train_pred = output.argmax(dim =1, Keepdim = True)
+            train_corr += train_pred.eq(target.view_as(train_pred)).sum().item()
+            train_loss += F.nll_loss(output,target,reduction='sum').item()
+            loss.backward()
+            optimiser.step()
+            g_step +=1
+            ema(model,g_step)
+            
